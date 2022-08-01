409813bcf8a535dfa50f16bbba9303e1345f2ba5
Rivaldo Esbend <esbendrivaldo@gmail.com>
2022-08-01 03:00:42 +0200

    Bootstrap going well

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,10 +2,18 @@ import React from 'react'
 
 export const Navbar = () => {
   return (
-    <div className='card' >
-      <div className='card-body'>
-        <h5 className='card-title'>Card title</h5>
-        <p className='card-text'>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <div className='container border border-dark border-1 mx-auto p-5 m-5' >
+      <div className='row'>
+        <div className='col-4 bg-primary'> Image</div>
+        <div className='col-6'>
+          <h3 className='card-title text-primary'>Rivaldo Esbend</h3><br></br>
+          <p className='card-text'>Hello, I appreciate you taking time out of your day to check my portfolio out. This portfolio includes my contact details, cv and some of the work I've done.</p>
+          <div>
+            <h5 className='card-title text-primary'>email: </h5>
+            <p className=''>words</p>
+          </div>
+          <h5 className='card-title text-primary'>phone: </h5>
+        </div>
       </div>
     </div>
   )
